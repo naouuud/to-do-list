972a9146c9fb9b9d6da21206f35360ec0b487ef4
Nour Aoude <naoude@gmail.com>
2023-01-31 16:49:57 +0200

    Fix form styling

--- src/item-form.js
+++ src/item-form.js
@@ -2,11 +2,7 @@ export function collectFormData() {
     const title = document.querySelector(".title input");
     const description = document.querySelector(".description textarea");
     const project = document.querySelector(".project select");
-    const priority = (function () {
-        let checked;
-        document.querySelectorAll(".priority input").forEach(element => { if (element.checked) checked = element });
-        return checked;
-    })();
+    const priority = document.querySelector(".priority input");
     const due = document.querySelector(".due input");
     return {
         title: title.value,
