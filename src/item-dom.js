7f21b3ce33d7717110f9d2262e2cfd0501dc535e
Nour Aoude <naoude@gmail.com>
2023-02-08 17:58:29 +0200

    Create list DOM

--- src/item-dom.js
+++ src/item-dom.js
@@ -6,6 +6,9 @@ export function renderItems() {
         document.querySelector(`#${project.name} .project-body .item-list`).innerHTML = "";
     })
     allItems.forEach(item => {
-        newElement({ type: "li", textContent: item.title, parent: `#${item.project} .project-body .item-list` });
+        const li = newElement({ type: "li", parent: `#${item.project} .project-body .item-list` });
+        const imgCheck = newElement({ type: "img", class: "item-check-icon unchecked", parent: li, src: "./../src/images/checkbox-blank-outline.svg", alt: "Item check icon (unchecked)" })
+        const span = newElement({ type: "span", textContent: item.title, parent: li });
+        const imgDelete = newElement({ type: "img", class: "item-delete-icon", parent: li, src: "./../src/images/delete-outline.svg", alt: "Item delete icon" })
     });
 }
